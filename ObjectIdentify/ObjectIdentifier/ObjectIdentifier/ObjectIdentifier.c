3b8f4e3ee86a0ccb20861091d39f6122bb5b6124
sebhero <trevligt.att.ses@gmail.com>
2017-04-06 15:03:41 +0200

    added atmel project for object identifying

---
+++ ObjectIdentify/ObjectIdentifier/ObjectIdentifier/ObjectIdentifier.c
@@ -0,0 +1,24 @@
+/*
+ * ObjectIdentifier.c
+ *
+ * Created: 2017-04-06 14:59:53
+ *  Author: trevl
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
