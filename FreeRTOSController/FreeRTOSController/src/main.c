a608b81bf33a163549a5c37929bb7b5fac7a8201
THDTA <me@example.com>
2017-04-07 09:30:20 +0200

    Working terminal

--- FreeRTOSController/FreeRTOSController/src/main.c
+++ FreeRTOSController/FreeRTOSController/src/main.c
@@ -6,6 +6,14 @@ int main (void)
 	sysclk_init();
 
 	board_init();
+	
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate = CONF_UART_BAUDRATE,
+		.paritytype = CONF_UART_PARITY
+	};
+	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+	stdio_serial_init(CONF_UART, &uart_serial_options);
+	
 
-	/* Insert application code here, after the board has been initialized. */
+	printf("Hello, World!\n");
 }
