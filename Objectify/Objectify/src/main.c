b85bcdbc263e947007139964d26659328a3acf03
Shvedi <jon_sona@yahoo.com>
2017-04-07 15:08:37 +0200

    Program for sonic in atmel

--- Objectify/Objectify/src/main.c
+++ Objectify/Objectify/src/main.c
@@ -29,9 +29,54 @@
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 #include <asf.h>
+#include <delay.h>
+#include <stdio.h>
+#include <stdio_serial.h>
 
-#define pin24 PIO_PA15_IDX
+#define trig PIO_PC26_IDX
+#define echo PIO_PC25_IDX
 
+
+
+void configureConsole()
+{
+	const usart_serial_options_t uart_serial_options = 
+	{
+		.baudrate = 115200,
+		.paritytype = UART_MR_PAR_NO
+	};
+	
+	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+	stdio_serial_init(CONSOLE_UART, &uart_serial_options);
+}
+
+
+long pulseIn()
+{
+	//printf("pulseread");
+	//timeout zone
+	unsigned long numloops = 0;
+	unsigned long maxloops = 5000000;
+	unsigned long width = 0;
+	// wait for any previous pulse to end
+	while ( ioport_get_pin_level(echo) == HIGH)
+	{
+		if (numloops++ == maxloops) break;
+	}
+	// wait for the pulse to start
+	while (ioport_get_pin_level(echo)== LOW)
+	{
+		if (numloops++ == maxloops) break;
+	}
+	
+	// wait for the pulse to stop @ here we are measuring the pulse width = incrementing the WIDTH value by one each cycle. atmega328 1 micro second is equal to 16 cycles.
+	while (ioport_get_pin_level(echo)==HIGH)
+	{
+		if (numloops++ == maxloops) break;
+		width++;
+	}
+	return width/5.1;
+}
 
 int main (void)
 {
@@ -39,8 +84,22 @@ int main (void)
 
 	sysclk_init();
 	board_init();
-
-	ioport_set_pin_dir(pin24,IOPORT_DIR_OUTPUT);
-
+	configureConsole();
+	ioport_set_pin_dir(trig,IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(echo, IOPORT_DIR_INPUT);
+	unsigned long duration, distance;
+	while(1)
+	{
+	
+ 		ioport_set_pin_level(trig, LOW);
+ 		delay_us(2);
+ 		ioport_set_pin_level(trig, HIGH);
+ 		delay_us(10);
+ 		ioport_set_pin_level(trig, LOW);
+ 		duration = pulseIn();
+ 		distance = duration / 58;
+		printf("%d cm\n", distance);
+		delay_ms(500);
+	}
 	/* Insert application code here, after the board has been initialized. */
 }
