a7dcd9879c1b0fe9cfba0b4f10fa9c02a1cb7f36
rakkagaming <robin.lennart.johnsson@gmail.com>
2017-04-10 13:34:19 +0200

    things arent working with kraken

--- Movement/GccBoardProject1/src/motorFunc.h
+++ Movement/GccBoardProject1/src/motorFunc.h
@@ -10,7 +10,13 @@
 #define MOTORFUNC_H_
 //Defines pin 24 on the Due-board as a pin to be used.
 #define pin24 PIO_PA15_IDX
+
+//Temporary "baseSpeed" that will be used until we get positioning and regulation
 #define baseSpeed 1750
+
+//Tested that this 1713 on the left wheel gives a kinda straight line when the other motor is 1750 (forward driving)
+#define baseSpeedLeft 1713
+
 #define reverseBaseSpeed 1250
 #define motorSwitch 1100
 #define timeOut 2000
@@ -24,8 +30,9 @@ void forwardDrive(void);
 void reverseDrive(void);
 void rotate(void);
 void stop(void);
+void turnLeft(void);
+void turnRight(void);
 void rotateByDegrees(uint8_t degree, uint16_t speed);
-
 
 
 
