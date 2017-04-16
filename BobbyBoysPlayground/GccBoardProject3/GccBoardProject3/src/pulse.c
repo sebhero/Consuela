86209a757602ea61a7bf5c6c810f4425269bdbd0
THDTA <me@example.com>
2017-04-16 22:45:13 +0200

    Pulse done

--- BobbyBoysPlayground/GccBoardProject3/GccBoardProject3/src/pulse.c
+++ BobbyBoysPlayground/GccBoardProject3/GccBoardProject3/src/pulse.c
@@ -5,73 +5,79 @@
  #include <asf.h>
  #include "pulse.h"
 
-
+ // Define the clock, see pwm_clock_t
  pwm_clock_t pulse_clock_setting = {
-     .ul_clka = 1000000L,
-     .ul_clkb = 0,
-     .ul_mck = BOARD_MCK // or CHIP_FREQ_CPU_MAX
+    .ul_clka = 1000000L,
+    .ul_clkb = 0,
+    .ul_mck = BOARD_MCK // or CHIP_FREQ_CPU_MAX
  };
 
- pulse_channel_t pulse_channel_0 = {
-    .pwm = PWM,
-    .pwm_channel = {
-        .channel = PWM_CHANNEL_0,
-        .ul_prescaler = PWM_CMR_CPRE_CLKA,
-        .ul_duty = 0,
-        .ul_period = 50000,
-        .polarity = PWM_HIGH
-    },
-    .id = ID_PWM,
-    .pin = PIO_PC3_IDX,
-    .mux = IOPORT_MODE_MUX_B
- };
-
- pulse_channel_t pulse_channel_1 = {
-    .pwm = PWM,
-    .pwm_channel = {
-        .channel = PWM_CHANNEL_1,
-        .ul_prescaler = PWM_CMR_CPRE_CLKA,
-        .ul_duty = 0,
-        .ul_period = 50000,
-        .polarity = PWM_HIGH
+ // Define the pulse channels, ie. the supporting PWM channels
+ pulse_channel_t pulse_channels[] = {
+    {
+        .pwm = PWM,
+        .pwm_channel = {
+            .channel = PWM_CHANNEL_0,
+            .ul_prescaler = PWM_CMR_CPRE_CLKA,
+            .ul_duty = 0,
+            .ul_period = 50000,
+            .polarity = PWM_HIGH
         },
-    .id = ID_PWM,
-    .pin = PIO_PC5_IDX,
-    .mux = IOPORT_MODE_MUX_B
+        .id = ID_PWM,
+        .pin = PIO_PC3_IDX,
+        .mux = IOPORT_MODE_MUX_B,
+        .dty_max = 2000
+    },
+    {
+        .pwm = PWM,
+        .pwm_channel = {
+            .channel = PWM_CHANNEL_1,
+            .ul_prescaler = PWM_CMR_CPRE_CLKA,
+            .ul_duty = 0,
+            .ul_period = 50000,
+            .polarity = PWM_HIGH
+        },
+        .id = ID_PWM,
+        .pin = PIO_PC5_IDX,
+        .mux = IOPORT_MODE_MUX_B,
+        .dty_max = 2000
+    }
  };
 
+ /*
+ * \brief Initialize the specified pulse channel
+ *
+ * \note Need to call pulse_start(ch_n) to start output
+ *
+ */
  static void pulse_init_channel(uint32_t ch_n) {
-    ioport_set_pin_mode(pulse_channels[ch_n]->pin, pulse_channels[ch_n]->mux);
-    ioport_disable_pin(pulse_channels[ch_n]->pin);
+    ioport_set_pin_mode(pulse_channels[ch_n].pin, pulse_channels[ch_n].mux);
+    ioport_disable_pin(pulse_channels[ch_n].pin);
 
-    pmc_enable_periph_clk(pulse_channels[ch_n]->id);
-    pwm_channel_disable(pulse_channels[ch_n]->pwm, pulse_channels[ch_n]->pwm_channel.channel);
-    pwm_init(pulse_channels[ch_n]->pwm, &pulse_clock_setting);
+    pmc_enable_periph_clk(pulse_channels[ch_n].id);
+    pwm_channel_disable(pulse_channels[ch_n].pwm, pulse_channels[ch_n].pwm_channel.channel);
+    pwm_init(pulse_channels[ch_n].pwm, &pulse_clock_setting);
  }
 
 
-static void initialize_pulse_channels(void) {
-
-    pulse_channels[0] = &pulse_channel_0;
-    pulse_channels[1] = &pulse_channel_1;
-
+void pulse_init() {
     pulse_init_channel(0);
     pulse_init_channel(1);
 }
 
-void pulse_init() {
-    initialize_pulse_channels();
-}
-
 void pulse_start(uint32_t ch_n) {
-    pwm_channel_init(pulse_channels[ch_n]->pwm, &(pulse_channels[ch_n]->pwm_channel));
-    pwm_channel_enable(pulse_channels[ch_n]->pwm, pulse_channels[ch_n]->pwm_channel.channel);
+    pwm_channel_init(pulse_channels[ch_n].pwm, &(pulse_channels[ch_n].pwm_channel));
+    pwm_channel_enable(pulse_channels[ch_n].pwm, pulse_channels[ch_n].pwm_channel.channel);
 }
 
 void pulse_stop(uint32_t ch_n) {
-    pwm_channel_enable(pulse_channels[ch_n]->pwm, pulse_channels[ch_n]->pwm_channel.channel);
+    pwm_channel_enable(pulse_channels[ch_n].pwm, pulse_channels[ch_n].pwm_channel.channel);
 }
 
-void pulse_set_period(uint32_t ch_n, uint32_t period_us) {
-    pwm_channel_update_duty(pulse_channels[ch_n]->pwm, &(pulse_channels[ch_n]->pwm_channel), period_us);
+uint32_t pulse_set_period(uint32_t ch_n, uint32_t period_us) {
+    if(period_us > pulse_channels[ch_n].dty_max) {
+        return 1;
+    }
+    pwm_channel_update_duty(pulse_channels[ch_n].pwm, &(pulse_channels[ch_n].pwm_channel), period_us);
+    return 0;
 }
--- BobbyBoysPlayground/GccBoardProject3/GccBoardProject3/src/pulse.h
+++ BobbyBoysPlayground/GccBoardProject3/GccBoardProject3/src/pulse.h
@@ -1,45 +1,68 @@
-/*
- * pulse.h
+/*!
+ * \file pulse.h
  *
  */
-
 
 #ifndef PULSE_H_
 #define PULSE_H_
 
 #include <asf.h>
 
-
-
-//pwm_channel_t pwm_channel_0 = {
-    //.channel = PWM_CHANNEL_0,
-    //.ul_prescaler = PWM_CMR_CPRE_CLKA,
-    //.ul_duty = 0,
-    //.ul_period = 50000,
-    //.polarity = PWM_HIGH
-//};
-//
-//pwm_channel_t pwm_channel_1 = {
-    //.channel = PWM_CHANNEL_1,
-    //.ul_prescaler = PWM_CMR_CPRE_CLKA,
-    //.ul_duty = 0,
-    //.ul_period = 50000,
-    //.polarity = PWM_HIGH
-//};
-
+/*
+* \brief Parameter container for pulse channel
+*/
 typedef struct {
+    // The PWM instance to use
     Pwm *pwm;
+    // see pwm_channel_t
     pwm_channel_t pwm_channel;
+    // Pulse channel ID
     uint32_t id;
+    // Pulse channel output pin
     ioport_pin_t pin;
+    // Pulse channel peripheral MUX
     ioport_mode_t mux;
+    // Pulse channel period in ms
+    uint32_t dty_max;
 } pulse_channel_t;
 
-pulse_channel_t *pulse_channels[2];
 
+/*
+* \brief Initialize the pulse channels
+*
+* \note Need to call pulse_start(ch_n) to start the pulse output
+*
+*/
 void pulse_init(void);
+
+/*
+* \brief Starts pulse generation on the specified channel
+*
+* \param ch_n Pulse channel number
+*
+*/
 void pulse_start(uint32_t ch_n);
+
+/*
+* \brief Stops pulse generation on the specified channel
+*
+* \note Call pulse_start(ch_n) to start the pulse output again
+*
+* \param ch_n Pulse channel number
+*
+*/
 void pulse_stop(uint32_t ch_n);
-void pulse_set_period(uint32_t ch_n, uint32_t period_us);
+
+/*
+* \brief Set the pulse period on the specified channel to the specified period in us
+*
+* \param ch_n Pulse channel number
+* \param period_us Pulse high period in microseconds
+*
+* \retval 0 Success
+* \retval 1 Pulse period too long
+*
+*/
+uint32_t pulse_set_period(uint32_t ch_n, uint32_t period_us);
 
 #endif /* PULSE_H_ */
