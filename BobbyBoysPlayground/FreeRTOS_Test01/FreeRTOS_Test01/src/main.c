a88868d10055ebf9f9bb767eade0286eefbb08fe
THDTA <me@example.com>
2017-05-03 13:23:42 +0200

    Test

--- BobbyBoysPlayground/FreeRTOS_Test01/FreeRTOS_Test01/src/main.c
+++ BobbyBoysPlayground/FreeRTOS_Test01/FreeRTOS_Test01/src/main.c
@@ -1,40 +1,24 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
 
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
 #include <asf.h>
+
+void configure_console() {
+    const usart_serial_options_t uart_serial_options = {
+        .baudrate = CONF_UART_BAUDRATE,
+        .paritytype = CONF_UART_PARITY
+    };
+    sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+    stdio_serial_init(CONF_UART, &uart_serial_options);
+}
 
 int main (void)
 {
-	/* Insert system clock initialization code here (sysclk_init()). */
+    sysclk_init();
 
 	board_init();
 
-	/* Insert application code here, after the board has been initialized. */
+    configure_console();
+
+	while(1) {
+        // spin
+    }
 }
