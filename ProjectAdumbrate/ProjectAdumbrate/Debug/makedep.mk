98a61130dd0345a1970fe366025b8ec2ec4239a3
danial2016 <danial_mahmoud@hotmail.com>
2017-05-08 23:32:19 +0200

    ProjectAdumbrate

--- ProjectAdumbrate/ProjectAdumbrate/Debug/makedep.mk
+++ ProjectAdumbrate/ProjectAdumbrate/Debug/makedep.mk
@@ -2,29 +2,11 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-src\buttonInterrupt.c
-
-src\pulseCounterHandler.c
-
 src\ASF\common\services\delay\sam\cycle_counter.c
 
 src\ASF\common\utils\stdio\read.c
 
 src\ASF\sam\drivers\usart\usart.c
-
-src\ASF\thirdparty\freertos\freertos-7.3.0\source\FreeRTOS_CLI.c
-
-src\ASF\thirdparty\freertos\freertos-7.3.0\source\list.c
-
-src\ASF\thirdparty\freertos\freertos-7.3.0\source\portable\gcc\sam\port.c
-
-src\ASF\thirdparty\freertos\freertos-7.3.0\source\portable\memmang\heap_4.c
-
-src\ASF\thirdparty\freertos\freertos-7.3.0\source\queue.c
-
-src\ASF\thirdparty\freertos\freertos-7.3.0\source\tasks.c
-
-src\ASF\thirdparty\freertos\freertos-7.3.0\source\timers.c
 
 src\ASF\thirdparty\wireless\addons\sio2host\uart\sio2host.c
 
@@ -33,6 +15,12 @@ src\ASF\common\utils\stdio\write.c
 src\ASF\common\services\serial\usart_serial.c
 
 src\ASF\sam\drivers\uart\uart.c
+
+src\pulseCounterHandler.c
+
+src\RealTimeTimer.c
+
+src\TimerCounter.c
 
 src\ASF\common\services\clock\sam3x\sysclk.c
 
