a12cbd62ef6f71b89b9ea084ed4bca2de9501ef1
rakkagaming <robin.lennart.johnsson@gmail.com>
2017-05-08 11:08:32 +0200

    Added comments to Navigation.c

--- Movement/GccBoardProject1/src/navigation.c
+++ Movement/GccBoardProject1/src/navigation.c
@@ -3,39 +3,55 @@
  *
  * Created: 03/05/2017 08:59:50
  *  Author: Robin Johnsson
+ *
+ *	Contains the navigation functions used by the platform.
+ *	The functions use angles, positions and distances to calculate what should be done.
  */ 
 #include "navigation.h"
 
-//Dummy-värden för nuvarande
+//Start of variables
+//Coordinates of the platform
 uint16_t x1_pos = 0;
 uint16_t y1_pos = 0;
 uint16_t x2_pos = 0;
 uint16_t y2_pos = 0;
 
+//Can be removed?
 uint8_t data[8];
 uint8_t temp;
+
+
 double distanceLeft;
 
+//Calculation variables
 int deltaX;
 int deltaY;
+
+//Different angle variables
 int angle;
 double angleRad;
-
 int platformAngle;
 int objectAngle;
 int angleVal;
 int angleTemp;
-
-static uint16_t mid_x;
-static uint16_t mid_y;
 int currentAngle = 90;
 
+//Variables for the former position of the platform
+uint16_t last_x;
+uint16_t last_y;
+
+//Mid values of the platform, will be x1 and y1 if only one set of coordinates is given
+static uint16_t mid_x;
+static uint16_t mid_y;
+
+//Struct used to define objects/positions
 typedef struct {
 	uint16_t x_pos;
 	uint16_t y_pos;
 	const char *name_p;
 	} object_pos_t;
 
+//Array of objects
 object_pos_t objects[] = {
 	{
 		.x_pos = sock_x,
@@ -60,54 +76,66 @@ object_pos_t objects[] = {
 };
 
 /*
-	Function that calls the navigation-unit and receives the data representing coordinates.
-	This then gets placed at the correct positions and the values can then be used.
+	Calculates the midpoint of the platform with the help of two sets of coordinates.
+	In the case that only one set is given, that will be used as the midpoint.
 */
-
-void callForData(uint16_t x1, uint16_t x2){
-	//data;//Function call
-	x1_pos = x1;
-	y1_pos = x2;
-	/*
-	x1_pos = (data[0] << 8) | (data[1] << 0);
-	
-	y1_pos = (data[2] << 8) | (data[3] << 0);
-	
-	x2_pos = (data[4] << 8) | (data[5] << 0);
-	
-	y2_pos = (data[6] << 8) | (data[7] << 0);*/
-}
-
 void calcMidPos(){
-	/*	Används då navigeringsgruppen ger oss 2 sätt av koordinater
+	/*	This part is used if 2 sets of coordinates are given
 		mid_x = (x1_pos+x2_pos)/2;
 		mid_y = (y1_pos+y2_pos)/2;
 	*/
+	//Use this part if only 1 set of coordinates are given.
 	mid_x = x1_pos;
 	mid_y = y1_pos;
 	
 }
 
+/*
+	Calculates the distance from the platform to a given object
+*/
 double distanceToPosition(uint8_t obj){
+	//Call that calculates angle of the object and X/Y differences
 	valuesCalc(obj);
+	//Calculates the hypotenuse 
 	distanceLeft = sqrt(pow(deltaX,2)+pow(deltaY,2));
+	//Returns the distance/hypotenuse
 	return distanceLeft;
 }
-
+/*
+	Calculate the angle from the platform to the given position/object.
+	Returns a negative number if the angle is closer clockwise than anti-clockwise
+*/
 int angleToPos(){
+	//Sets the current angle into a temp variable
 	platformAngle = currentAngle;
+	//Transforms the angle to the objective to a usable value(0-360)
 	objectAngle = 180 - angle;
+	
+	//Calculates the angle-difference between the platform and the objective
 	angleVal = abs(((abs(platformAngle-360) + objectAngle)%360) - 360);
 	
+	//If the angle-difference is greater than 180, make it negative
 	(angleVal > 180) ? (angleVal -= 360) : (0);
+	
 	return angleVal;
 }
 
+/*
+	Calculates the difference in X/Y between the platform and a given position
+	Also calculates the angle to the object from the platform
+*/
 void valuesCalc(uint8_t obj){
+	//Calculates the position of the platform
 	calcMidPos();
+	
+	//Calculates the X/Y difference
 	deltaX = objects[obj].x_pos - mid_x;
 	deltaY = objects[obj].y_pos - mid_y;
+	
+	//Calculates the angle between the platform and object in radians
 	angleRad = atan2(deltaY,deltaX);
+	
+	//Converts the angle from radians to degrees
 	angle = (angleRad*180)/PI;
 }
 
@@ -119,6 +147,9 @@ void updateAngle(){
 	currentAngle = ((currentAngle - angleVal)+360)%360;
 }
 /*
+	Updates the position of the platform by calculating the distance traveled along with 
+	the angle traveled in.
+	
 	This function is only used when Dead Reckoning is being used.
 	When fetching the location data from an external source instead 
 	use "callForData" and update the position!
@@ -131,34 +162,61 @@ void updatePos(double hyp){
 	y1_pos = mid_y;
 }
 
-uint16_t last_x;
-uint16_t last_y;
+/*
+	Checks if the angle that the platform actually travels in is the same as the believed one.
+	If they differ to much, the platform will stop and recalibrate to fix this.
+	
+	Still not tested! 
+*/
 void angleCheck(){
+	//Saves the last position
 	last_x = mid_x;
 	last_y = mid_y;
+	
+	//Calculates the new position of the platform
 	calcMidPos();
+	
+	//Calculates the X/Y-difference of the platform and the object
 	deltaX = mid_x - last_x;
 	deltaY = mid_y - last_y;
+	
+	//Calculates the angle to the object from the platform
 	angleRad = atan2(deltaY,deltaX);
 	angleTemp = (angleRad*180)/PI;
+	angleTemp = 180 - angleTemp;
 	
-	if (abs(angle-currentAngle)>4)
+	//If the angle-difference of the current travel-angle and the angle to the object is bigger than 4
+	//then fix the positioning so that the angle is equal.
+	if (abs(angleTemp-currentAngle)>4)
 	{
+		//Stops the movement
 		stop();
-		currentAngle = angle;
+		//Sets currentAngle to the actual angle of the platform
+		currentAngle = angleTemp;
+		
+		//Rotates the platform the needed amount in the correct direction.
 		rotationChooser(angleToPos());
 		
 	}
 	
 }
 
+/*
+	Choose the direction of the rotation depending on the value of input
+*/
 void rotationChooser(int degreesToPos){
+	//If the value is negative rotate right, else left
 	if (degreesToPos<0){
+		//Makes the value into a positive value
 		degreesToPos = abs(degreesToPos);
+		//Rotates the platform clockwise by the input value
 		rotateRightByDegrees(degreesToPos);
+		//Update the angle
 		updateAngle();
 		} else{
+		//Rotates the platform anti-clockwise by the input value
 		rotateLeftByDegrees(degreesToPos);
+		//Update the angle
 		updateAngle();
 	}
 }
