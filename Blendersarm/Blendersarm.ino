318f509868ec2728a67b785807487d8e4b48f6d3
Vuxenkalas <viktormalmgren@hotmail.com>
2017-05-16 14:32:50 +0200

    Senaste rotationregleringen + blenders

---
+++ Blendersarm/Blendersarm.ino
@@ -0,0 +1,311 @@
+//
+// Created by Sebastian Boreback on 2017-05-12.
+//
+
+#include <Wire.h>
+
+int breakpin = 9;
+int directionpin = 12;
+int pwmpin = 3;
+int breakTop = 4;
+int breakBot = 7;
+
+typedef struct ArmInfo {
+  //distances are in cm
+  uint8_t boxDistance;
+  //angles in degrees
+  uint8_t boxAngle;
+  uint8_t objectDistance;
+  uint8_t objectAngle;
+  //Boolean. true that arm can collect all obj. then dropoff
+  //false if need to dropoff each obj. after pickup
+  uint8_t collectAll;
+
+} arminfo_t;
+
+//TWI state
+typedef enum TWI_CMD {
+
+  TWI_CMD_ARM_INIT = 0x20,
+  TWI_CMD_DROPOFF_START = 0x21,
+  TWI_CMD_PICKUP_START = 0x22,
+  TWI_CMD_DROPOFF_STATUS = 0x23,
+  TWI_CMD_PICKUP_STATUS = 0x24,
+  TWI_CMD_ERROR = 0x25,
+} twi_cmd;
+
+typedef enum TWI_CMD_INIT_REQ {
+  TWI_CMD_ARM_REQ_BOX_INFO = 2,
+  TWI_CMD_ARM_REQ_OBJ_INFO = 3,
+  TWI_CMD_ARM_REQ_COLLECT_INFO = 4
+} twi_cmd_init_req;
+
+typedef enum PICKUPSTATUS {
+  PICKUP_DONE = 2,
+  PICKUP_FORWARD = 3,
+  PICKUP_BACKWARD = 4,
+  PICKUP_RUNNING = 5,
+  PICKUP_FAILED = 6,
+  PICKUP_DONE_DRIVE = 7,
+  PICKUP_IDLE = 8
+} PICKUP_STATUS;
+
+typedef enum DROPOFFSTATUS {
+  DROPOFF_DONE = 2,
+  DROPOFF_RUNNING = 3,
+  DROPOFF_FAILED = 4,
+  DROPOFF_IDLE = 5
+} DROPOFF_STATUS;
+
+typedef enum object {
+  SOCK = 2,
+  SQUARE = 3,
+  GLASS = 4
+} OBJECT;
+
+typedef enum FSMSTATES {
+  ARM_IDLE = 2,
+  ARM_PICKUP = 3,
+  ARM_DROPOFF = 4
+} STATE;
+
+STATE currentState = ARM_IDLE;
+STATE nextState;
+
+/***********************/
+//variables
+uint8_t recivedData[3] = {0};
+uint8_t txBuff[3] = {0};
+arminfo_t theArm;
+PICKUP_STATUS pickupStatus;
+DROPOFF_STATUS dropoffStatus;
+uint8_t run;
+uint8_t doneBack;
+uint8_t doneFrwd;
+
+
+void handleReadCmd() {
+
+  //data to send
+  //uint8_t data[3] = {};
+  //Serial.println("handleReadCmd");
+
+  uint8_t cmd = recivedData[0];
+
+  switch (cmd) {
+    case TWI_CMD_ARM_INIT:
+      //Serial.println("TWI_CMD_ARM_INIT");
+      txBuff[0] = cmd;
+      switch (recivedData[1]) {
+        case TWI_CMD_ARM_REQ_BOX_INFO:
+          //Serial.println("TWI_CMD_ARM_REQ_BOX_INFO");
+          txBuff[1] = theArm.boxDistance;
+          txBuff[2] = theArm.boxAngle;
+          break;
+        case TWI_CMD_ARM_REQ_OBJ_INFO:
+          //Serial.println("TWI_CMD_ARM_REQ_OBJ_INFO:");
+          txBuff[1] = theArm.objectDistance;
+          txBuff[2] = theArm.objectAngle;
+          break;
+        case TWI_CMD_ARM_REQ_COLLECT_INFO:
+          //Serial.println("TWI_CMD_ARM_REQ_COLLECT_INFO:");
+          txBuff[1] = theArm.collectAll;
+          break;
+        default:
+          Serial.println("ERR Unhandle armInfo");
+          break;
+      }
+      break;
+    case TWI_CMD_DROPOFF_START:
+      //Serial.println("TWI_CMD_DROPOFF_START");
+      //set nextstate to dropoff in FSM
+      nextState = ARM_DROPOFF;
+      dropoffStatus = DROPOFF_RUNNING;
+      txBuff[0] = cmd;
+      txBuff[1] = dropoffStatus;
+      break;
+    case TWI_CMD_PICKUP_START:
+      //start pickup in FSM, mainloop
+      nextState = ARM_PICKUP;
+      pickupStatus = PICKUP_RUNNING;
+      txBuff[0] = TWI_CMD_PICKUP_STATUS;
+      txBuff[1] = pickupStatus;
+      break;
+    case TWI_CMD_DROPOFF_STATUS:
+      //Serial.println("TWI_CMD_DROPOFF_STATUS");
+      txBuff[0] = cmd;
+      txBuff[1] = dropoffStatus;
+      break;
+    case TWI_CMD_PICKUP_STATUS:
+      //Serial.println("TWI_CMD_PICKUP_STATUS");
+      if (recivedData[1] == PICKUP_DONE_DRIVE) {
+
+        //wanted to drive back
+        if (pickupStatus == PICKUP_BACKWARD) {
+          doneBack = 1;
+        }
+        //wanted to drive forward
+        if (pickupStatus == PICKUP_FORWARD) {
+          doneFrwd = 1;
+        }
+        pickupStatus = (PICKUP_STATUS)recivedData[1];
+      }
+      txBuff[0] = TWI_CMD_PICKUP_STATUS;
+      txBuff[1] = pickupStatus;
+      break;
+    case TWI_CMD_ERROR:
+      Serial.println("TWI_CMD_ERROR");
+      break;
+    default:
+      printf("error\n");
+      break;
+  }
+}
+
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  //todo del
+  // Serial.println("sendREQ");
+  //Serial.println("sending");
+  //Serial.println(txBuff[0],HEX);
+  //Serial.println(txBuff[1]);
+  //Serial.println(txBuff[2]);
+  //todo del
+
+  Wire.write(txBuff, 3);
+  txBuff[0] = 0;
+  txBuff[1] = 0;
+  txBuff[2] = 0;
+
+
+  //end of request
+}
+
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  //  Serial.println("Handling new cmd Req");
+  // Serial.print("how many: ");
+  // Serial.println(howMany);
+
+  if (3 <= howMany) {
+    int i = 0;
+    while (Wire.available()) {
+      recivedData[i] = (uint8_t) Wire.read();
+      i++;
+    }
+
+    handleReadCmd();
+  }
+
+  //if its a command from master.
+  if (howMany <= 1) {
+    Wire.read();    // receive byte as an integer
+    //Serial.println("on or less");
+
+  }
+  //end of receive
+}
+
+
+void upp() {
+  while (!digitalRead(breakTop)) {
+    digitalWrite(breakpin, LOW);
+    digitalWrite(directionpin, LOW);
+    analogWrite(pwmpin, 255);
+  }
+  stoppmovement();
+}
+
+void ned() {
+
+  while (!digitalRead(breakBot)) {
+    digitalWrite(breakpin, LOW);
+    digitalWrite(directionpin, HIGH);
+    analogWrite(pwmpin, 255);
+  }
+  stoppmovement();
+}
+
+
+void startsug() {
+  digitalWrite(2, HIGH);
+}
+void  stoppsug() {
+  digitalWrite(2, LOW);
+}
+void stoppmovement() {
+  analogWrite(pwmpin, 0);
+}
+
+void setup() {
+  Wire.begin(2);                // join i2c bus with address #2
+  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
+
+  pinMode(2, OUTPUT);
+  pinMode(breakTop, INPUT_PULLUP);
+  pinMode(breakBot, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
+  pinMode(12, OUTPUT);
+
+
+  run = 1;
+
+  theArm.boxDistance = 30;//cm to goalbox
+  theArm.boxAngle = 20;//infront angle
+  theArm.objectDistance = 40;//distance from obj
+  theArm.objectAngle = 90;//from behind
+  theArm.collectAll = 1;//collect all
+
+  //todo for test remove
+  uint8_t doneBack = 0;
+  uint8_t doneFrwd = 0;
+  //todo remove end
+}
+
+void loop() {
+  //check if we are done
+  //since we are already in a loop
+
+  if (run) {
+    // Serial.println("IS running");
+    switch (currentState) {
+
+      case ARM_IDLE:
+        Serial.println("is idle");
+        delay(300);
+        //nextState = ARM_IDLE;
+        break;
+      case ARM_PICKUP:
+        pickupStatus = PICKUP_RUNNING;
+        ned();
+        startsug();
+        delay(5000);
+        upp();
+
+        pickupStatus = PICKUP_DONE;
+        nextState = ARM_IDLE;
+
+        
+        break;
+        
+      case ARM_DROPOFF:
+        ned();
+        stoppsug();
+        delay(5000);
+        upp();
+       
+        break;
+    }
+
+    delay(100);
+    currentState = nextState;
+  }
+
+}
+
--- Movement2/Movement2/src/Motorfunc.c
+++ Movement2/Movement2/src/Motorfunc.c
@@ -8,6 +8,7 @@
 #include "pulseCounterHandler.h"
 #include "buttonInterrupt.h"
 #include "pulse.h"
+#include "TimerCounter.h"
 #define pulseh_ch 0
 #define pulsev_ch 1
 #define pulseh_timer 0
@@ -20,17 +21,23 @@ void stop(){
 }
 
 void rotateRightByDegrees(double degree){
-
+	
 	stop();
-	degree=degree*1.1;
-	degree=(degree/4)-1;
+	//double engradtal = 459461.4694444444;
+
+	//degree=degree*engradtal;
 	
-	pulse_set_period(pulseh_ch, 1290); 
-	pulse_set_period(pulsev_ch, 1660); 
-	
+	degree = degree/4;
+	pulse_set_period(pulseh_ch, 1365); 
+	pulse_set_period(pulsev_ch, 1606);
+	//uint32_t startspin  = 0;
+	//TC0_init();
+	//startspin = 0;	
 	counterA = 0;
-	counterB = 0;
-	while((counterA<degree)){
+	counterB = 0; 
+	//while((read_counter_value()<degree)){	
+	//}
+	while(counterA<degree){
 		delay_ms(1);
 	}
 	stop();
@@ -41,11 +48,17 @@ void rotateRightByDegrees(double degree){
 void rotateLeftByDegrees(double degree){
 
 	stop();
-	degree = degree*1.05;
-	degree=(degree/4)-1;
+	//degree = degree*1.05;
+	//degree=(degree/4)-1;
 	
-	pulse_set_period(pulseh_ch, 1700);
-	pulse_set_period(pulsev_ch, 1310);
+	degree = degree*1;
+	degree=(degree/4);
+	
+	//pulse_set_period(pulseh_ch, 1700);
+	//pulse_set_period(pulsev_ch, 1310);
+	
+	pulse_set_period(pulseh_ch, 1635);
+	pulse_set_period(pulsev_ch, 1355);
 	
 	counterA = 0;
 	counterB = 0;
